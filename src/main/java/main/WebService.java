c204973a0b81741fce00acfb8b69e061f6765b50
Jose Manuel Martinez <2015378@ce.pucmm.edu.do>
2019-12-17 02:19:24 -0400

    non functional updates

---
+++ src/main/java/main/WebService.java
@@ -0,0 +1,91 @@
+package main;
+import com.buck.common.codec.Codec;
+import entidades.URL;
+import entidades.Usuario;
+import freemarker.template.Configuration;
+import services.*;
+import utils.JsonUtils;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.sql.Timestamp;
+
+import static spark.Spark.*;
+//import static spark.debug.DebugScreen.enableDebugScreen;
+public class WebService {
+    public final static String ACCEPT_TYPE_JSON = "application/json";
+    public final static String ACCEPT_TYPE_XML = "application/xml";
+    public final static int BAD_REQUEST = 400;
+    public final static int ERROR_INTERNO = 500;
+    private static String contrasenia = "proyectoFinal";
+
+    public static void main(String[] args) throws Exception{
+
+        DataBaseService.getInstance().iniciarDb();
+        Configuration configuration = new Configuration(Configuration.getVersion());
+        configuration.setClassForTemplateLoading(Main.class, "/templates");
+        createEntities();
+//        enableDebugScreen();
+        Codec codec = Codec.forName("Base16");
+        Arranque.init();
+
+        afterAfter("/*", (request, response) -> {
+            response.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+            response.header("Access-Control-Allow-Origin", "*");
+            response.status(200);
+        });
+
+        path("/api", () -> {
+            Spark.get("/", (request, response) -> {
+                return "Empezando Acrotador URL";
+            }, JsonUtils.json());
+//            Spark.post("/agregandoUrl", Main.ACCEPT_TYPE_JSON, (request, response) -> {
+//                System.out.println(request.body());
+//                UrlS url = new Gson().fromJson(request.body(), UrlS.class);
+//                Shortener u = new Shortener(5, "www.tinyurl.com/");
+//                url.setHashMaked(u.getKey(url.getUrl()));
+//                UrlsService.getInstancia().crear(url);
+//                // Shortener u = new Shortener(5, "www..com/");
+//                return url;
+//            }, JsonUtils.json());
+        });
+    }
+
+    private static void createEntities(){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MiUnidadPersistencia");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Usuario adminUser = new Usuario(
+                "admin",
+                "admin",
+                "admin",
+                "admin",
+                true
+        );
+        Usuario guessUser = new Usuario(
+                "guess",
+                "guess",
+                "guess",
+                "guess",
+                false
+        );
+        URL urlInit = new URL(
+                "/rd/prueba",
+                "https://www.google.com.do/",
+                new Timestamp(System.currentTimeMillis()),
+                0,
+                adminUser
+        );
+        UsuarioService.getInstance().crear(adminUser);
+        UsuarioService.getInstance().crear(guessUser);
+        if(URLService.getInstance().selectUrlByUrlReferenciaAndUsuario(urlInit.geturlReferencia(), adminUser) == null){
+            URLService.getInstance().crear(urlInit);
+        }
+    }
+}
---
+++ src/main/java/soap/JettyStart.java
@@ -0,0 +1,35 @@
+package soap;
+
+import com.sun.net.httpserver.HttpContext;
+import org.eclipse.jetty.http.spi.HttpSpiContextHandler;
+import org.eclipse.jetty.http.spi.JettyHttpContext;
+import org.eclipse.jetty.http.spi.JettyHttpServer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import sun.rmi.transport.Endpoint;
+
+import java.lang.reflect.Method;
+
+public class JettyStart {
+    public static void init() throws Exception {
+        Server server = new Server(963);
+        ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+        server.setHandler(contextHandlerCollection);
+        server.start();
+
+        HttpContext context = build(server, "/ws");
+
+        UsuarioWebService usuarioWebService = new UsuarioWebService();
+        Endpoint endpoint = Endpoint.create(usuarioWebService);
+        endpoint.publish(context);
+    }
+    private static com.sun.net.httpserver.HttpContext build(Server server, String contextString) throws Exception {
+        JettyHttpServer jettyHttpServer = new JettyHttpServer(server, true);
+        JettyHttpContext ctx = (JettyHttpContext) jettyHttpServer.createContext(contextString);
+        Method method = JettyHttpContext.class.getDeclaredMethod("getJettyContextHandler");
+        method.setAccessible(true);
+        HttpSpiContextHandler contextHandler = (HttpSpiContextHandler) method.invoke(ctx);
+        contextHandler.start();
+        return ctx;
+    }
+}
